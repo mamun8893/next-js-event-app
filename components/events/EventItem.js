c28f15616804a8c910087868e9df2af59fc5bab3
mamun8893 <0316361936@student.wub.edu.bd>
2022-06-27 23:49:22 +0600

    feat:custom button component add

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,4 +1,8 @@
 import Link from "next/link";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
+import DateIcon from "../icons/date-icon";
+import Button from "../ui/Button";
 import classes from "./event-item.module.css";
 
 function EventItem({ event }) {
@@ -17,14 +21,21 @@ function EventItem({ event }) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formatedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={expolreLink}>Explore Event</Link>
+          <Button link={expolreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,4 +1,15 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
 function EventDetailsPage() {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <h2>Event not found</h2>;
+  }
+
   return <h2>Event Details Page</h2>;
 }
 
