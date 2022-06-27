7638765fdd49425726d3e62a0c2df0f7effeba16
mamun8893 <0316361936@student.wub.edu.bd>
2022-06-28 01:04:00 +0600

    design: event search

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,15 @@
 import EventList from "../../components/events/EventList";
+import EventSearch from "../../components/events/EventSearch";
 import { getAllEvents } from "../../dummy-data";
 
 function AllEventPage() {
   const events = getAllEvents();
-  return <EventList items={events} />;
+  return (
+    <>
+      <EventSearch />
+      <EventList items={events} />
+    </>
+  );
 }
 
 export default AllEventPage;
